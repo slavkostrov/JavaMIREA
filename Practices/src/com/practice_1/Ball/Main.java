dc18ec9a8fc2d0d48b46f27bed5496b69c2dfe57
Slava <slavkostrov@gmail.com>
2020-09-07 10:57:28 +0300

    init

---
+++ Practices/src/com/practice_1/Ball/Main.java
@@ -0,0 +1,12 @@
+package com.practice_1.Ball;
+
+public class Main {
+    public static void main(String[] args) {
+        Ball ball_1 = new Ball("Black", 10.5);
+        Ball ball_2 = new Ball();
+        Ball ball_3 = new Ball("White", 7.7);
+        System.out.println(ball_1);
+        System.out.println(ball_2);
+        System.out.println(ball_3);
+    }
+}
