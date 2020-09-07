ead059bc882b0eabf8635a4cff57e09668946bc5
Slava <slavkostrov@gmail.com>
2020-09-07 22:24:53 +0300

    practice_3

---
+++ Practices/src/com/practice_3/Circle.java
@@ -0,0 +1,35 @@
+package com.practice_3;
+
+public class Circle extends Shape{
+    private double radius;
+    public Circle(){
+        this.radius = 10;
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled){
+        this.color = color;
+        this.filled = filled;
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+    @Override
+    public double getPerimetr(){
+        return 2*Math.PI*radius;
+    }
+    @Override
+    public String toString(){
+        return "Shape: circle, radius: "+this.radius+", color: "+this.color;
+    }
+}
