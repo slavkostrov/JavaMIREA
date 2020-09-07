f495c2664da3d459ed429e84f160d290d12ee485
Slava <slavkostrov@gmail.com>
2020-09-07 23:09:11 +0300

    ex_3

---
+++ Practices/src/com/practice_3/Movable.java
@@ -0,0 +1,8 @@
+package com.practice_3;
+
+public interface Movable {
+    public abstract void moveDown();
+    public abstract void moveLeft();
+    public abstract void moveRight();
+    public abstract void moveUp();
+}
