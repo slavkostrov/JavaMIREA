1908496c59c9fbe6a7b1b2e04c2e43c97705e583
Slava <slavkostrov@gmail.com>
2020-09-07 10:04:45 +0300

    First commit

---
+++ JavaProjects/src/com/Kostrov/practice_1/Main.java
@@ -0,0 +1,15 @@
+package com.Kostrov.practice_1;
+
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) {
+        Dog d1 = new Dog("Mike", 2);
+        Dog d2 = new Dog("Helen", 7);
+        Dog d3 = new Dog("Bob");
+        d3.setAge(1);
+        System.out.println(d1);
+        d1.intoHumanAge();
+        d2.intoHumanAge();
+        d3.intoHumanAge();
+    }
+}
