25532556928d5687e3fd1a0c2be02a96c3368ef2
Slava <slavkostrov@gmail.com>
2020-09-08 22:29:12 +0300

    fixes

--- Practices/src/com/practice_4/app/application.java
+++ Practices/src/com/practice_4/app/application.java
@@ -8,32 +8,49 @@ class application extends JFrame {
 
     JButton button_1 = new JButton("AC Milan");
     JButton button_2 = new JButton("Real Madrid");
-    JLabel result = new JLabel("Result: 0 X 0");
-    JLabel last_scorer = new JLabel("Last scorer: N/A");
+    JLabel result = new JLabel("Result: 0 X 0", JLabel.CENTER);
+    JLabel last_scorer = new JLabel("Last scorer: N/A", JLabel.CENTER);
+    JLabel winner = new JLabel("Draw", JLabel.CENTER);
     private int milan_score = 0;
     private int madrid_score = 0;
     Font fnt = new Font("Times new roman", Font.BOLD, 20);
-
     application() {
         super("Practice 3");
-        setLayout(new FlowLayout());
-        setSize(250, 150);
-        add(result,  BorderLayout.EAST);
-        add(last_scorer,  BorderLayout.WEST);
-        add(button_1);
-        add(button_2);
+        setLayout(new BorderLayout());
+        setSize(400, 200);
+        setResizable(false);
+        add(winner,  BorderLayout.NORTH);
+        add(result,  BorderLayout.CENTER);
+        add(last_scorer,  BorderLayout.SOUTH);
+        add(button_1, BorderLayout.EAST);
+        add(button_2, BorderLayout.WEST);
         button_1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 milan_score += 1;
-                result.setText("Result: " + milan_score + " X " + madrid_score);
-                last_scorer.setText("AC Milan");
+                result.setText("Result: " + milan_score + " X " + madrid_score + " ");
+                last_scorer.setText("Last scorer: AC Milan");
+                if (madrid_score < milan_score){
+                    winner.setText("AC Milan");
+                }
+                else if (milan_score == madrid_score)
+                {
+                    winner.setText("Draw");
+                }
             }
         });
         button_2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 madrid_score += 1;
                 result.setText("Result: " + milan_score + " X " + madrid_score);
-                last_scorer.setText("Real Madrid");
+                last_scorer.setText("Last scorer: Real Madrid");
+                if (madrid_score > milan_score){
+                    winner.setText("Real Madrid");
+                }
+                else if (milan_score == madrid_score)
+                {
+                    winner.setText("Draw");
+                }
+
             }
         });
         setVisible(true);
