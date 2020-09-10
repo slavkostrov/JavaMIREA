733f9d337e6a39f1b21c613515246db1c05b26db
Slava <slavkostrov@gmail.com>
2020-09-11 00:31:25 +0300

    ones fix

--- Practices/src/com/practice_5/ones.java
+++ Practices/src/com/practice_5/ones.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 
 public class ones {
     private static Scanner in;
-    public static int counter(int prev){
+    public static int counter(){
         int x = in.nextInt();
-        if (x == prev && x == 0)
-            return 0;
+        if (x == 0) {
+            int y = in.nextInt();
+            if (y == 0)
+                return 0;
+            if (y == 1)
+                return 1 + counter();
+            else
+                return counter();
+        }
         else
-            if (x==1)
-                return 1 + counter(x);
+            if (x == 1)
+                return 1 + counter();
         else
-            return counter(x);
+            return counter();
     }
 
     public static void main(String[] args) {
         in = new Scanner(System.in);
-        int answer = counter(1);
+        int answer = counter();
         System.out.println(answer);
     }
 }
