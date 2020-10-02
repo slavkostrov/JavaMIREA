02f2ef2744047412900e1e91a409d037de8cc083
Slava <slavkostrov@gmail.com>
2020-10-02 22:14:20 +0300

    GPA

--- Practices/src/com/practice_6/Comparator_test.java
+++ Practices/src/com/practice_6/Comparator_test.java
@@ -1,11 +1,22 @@
 package com.practice_6;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Comparator_test
 {
 
     public static void main(String[] args) {
+        Student[] list = new Student[20];
+        System.out.println("Quick sort: ");
+        for (int i = 0; i < 20; i++)
+            list[i] = new Student(0, 9 - i, "unk");
+        Arrays.sort(list, new SortingStudentsByGPA());
+        for (Student i: list){
+            System.out.println(i);
+        }
+        System.out.println("\n");
+        System.out.println("Tree set example: ");
         SortingStudentsByGPA comp = new SortingStudentsByGPA();
         TreeSet<Student> students = new TreeSet<Student>(comp);
         students.add(new Student(1, 10, "Mawa"));
--- Practices/src/com/practice_6/SortingStudentsByGPA.java
+++ Practices/src/com/practice_6/SortingStudentsByGPA.java
@@ -4,14 +4,15 @@ import java.util.Comparator;
 
 public class SortingStudentsByGPA implements Comparator <Student>{
 
-
     @Override
     public int compare(Student o1, Student o2) {
         if (o1.getAverage() < o2.getAverage())
-            return -1;
+            return 1;
         else if (o1.getAverage() == o2.getAverage())
             return 0;
         else
-            return 1;
+            return -1;
     }
+
+
 }
